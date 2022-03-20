db753b73912d4991ccd5f7ca0eee32b02cafdbc8
JustNotAFox <justnotafox@gmail.com>
2022-03-20 04:08:08 -0400

    Added functionality to parse keys out of party chat. Prints key level and dungeon name to chat window to confirm

--- main.lua
+++ main.lua
@@ -1,22 +1,38 @@
-function initData()
-	if AccountKeyScores == nil then
-		AccountKeyScores = {};
+function MPAInitData()
+	if MPAAccountKeyScores == nil then
+		MPAAccountKeyScores = {};
 	end
 	currChar = UnitName("player");
-	if AccountKeyScores[currChar] == nil then
-		AccountKeyScores[currChar] = {};
+	if MPAAccountKeyScores[currChar] == nil then
+		MPAAccountKeyScores[currChar] = {};
 	end
 	mapIDList = C_ChallengeMode.GetMapTable();
 	for i, mapID in pairs(mapIDList) do
 		KeyScores, KeyBest = C_MythicPlus.GetSeasonBestAffixScoreInfoForMap(mapID)
-		AccountKeyScores[currChar][mapID] = KeyScores
+		MPAAccountKeyScores[currChar][mapID] = KeyScores
 	end
 end
-local EventFrame = CreateFrame("frame", "EventFrame")
-EventFrame:RegisterEvent("PLAYER_ENTERING_WORLD")
---EventFrame:RegisterEvent("CHAT_MSG_PARTY")
-EventFrame:SetScript("OnEvent", function(self, event, ...)
-	if event == "PLAYER_ENTERING_WORLD" then
-		initData()
+function MPAChatParse(text)
+	local mapString, keyLevel = string.match(text, '|Hkeystone:%d+:(%d+):(%d+)')
+	if mapString == nil or keyLevel == nil then
+		return
 	end
+	local keyDungeonName = C_ChallengeMode.GetMapUIInfo(mapString)
+	print("Found keystone", keyLevel, keyDungeonName)
+end
+local MPAEventFrame, MPAEventHandlers = CreateFrame("frame", "EventFrame"), {}
+function MPAEventHandlers:PLAYER_ENTERING_WORLD()
+	MPAInitData()
+end
+function MPAEventHandlers:CHAT_MSG_PARTY(text, ...)
+	MPAChatParse(text)
+end
+function MPAEventHandlers:CHAT_MSG_PARTY_LEADER(text, ...)
+	MPAChatParse(text)
+end
+for event, handler in pairs(MPAEventHandlers) do
+	MPAEventFrame:RegisterEvent(event)
+end
+MPAEventFrame:SetScript("OnEvent", function(self, event, ...)
+	MPAEventHandlers[event](self, ...)
 end)
